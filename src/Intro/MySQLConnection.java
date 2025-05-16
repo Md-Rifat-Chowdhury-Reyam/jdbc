116ef2f01c9d6ae90413be1f3330e539c4da8592
Md. Rifat Chowdhury <152366467+Md-Rifat-Chowdhury-Reyam@users.noreply.github.com>
2025-05-16 19:36:59 +0600

    jdbc connection

---
+++ src/Intro/MySQLConnection.java
@@ -0,0 +1,27 @@
+package Intro;
+
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    public static void main(String[] args) {
+
+        String url = " ";
+        String userName = " ";
+        String password =  "  ";
+        String Query = " ";
+
+        try {
+
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println(e.getException());
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+}
